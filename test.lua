45e59163cbd7542c24ff4792a53a7b96b2300852
Steffan <steffan@mail.com>
2025-07-14 10:34:55 +0200

    Added test code

--- test.lua
+++ test.lua
@@ -1,3 +1,15 @@
---[[
-Testing github
---]]
+-- MARIO PROBLEM FROM CS50
+-- Solved in lue
+
+local height = 4
+
+for i=1, height do
+    for j=1, height do
+        if j <= (height - i) then
+            io.write(" ")
+        else
+            io.write("#")
+        end
+    end
+    io.write("\n")
+end
