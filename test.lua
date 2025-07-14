356774da391aaea507403f3c0d2ab75e6895b32b
Steffan <steffan@mail.com>
2025-07-14 10:49:34 +0200

    Added mario-more and user input

--- test.lua
+++ test.lua
@@ -1,7 +1,14 @@
 -- MARIO PROBLEM FROM CS50
 -- Solved in lue
 
-local height = 4
+local height = 0
+
+while height < 1 or height > 8 do
+    io.write("Enter a number between 1-8: ")
+    height = tonumber(io.read())
+
+    if not height then height = 0 end
+end
 
 for i=1, height do
     for j=1, height do
@@ -11,5 +18,13 @@ for i=1, height do
             io.write("#")
         end
     end
+
+    -- MARIO MORE
+    io.write("  ")
+    for k=1, i do
+        io.write("#")
+    end
+    -- MARIO MORE
+
     io.write("\n")
 end
